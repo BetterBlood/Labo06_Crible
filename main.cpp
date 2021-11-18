d7c946f595f7d58fb7d0f1a9b173e259c7c7252c
Pedro Alves da Silva <pedro.s@ig.com>
2021-11-18 16:52:04 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* 
+ ----------------------------------------------------------------------------------- 
+ Nom du fichier :  $fileRelativePath()$ $end$
+ Auteur(s)      : Pedro Alves da Silva
+ Date creation  : $date("dd-MM-YYYY")$
+ 
+ Description    : <à compléter> 
+ 
+ Remarque(s)    : <à compléter> 
+ 
+ Compilateur    : Mingw-w64 g++ 11.1.0  
+ ----------------------------------------------------------------------------------- 
+*/
